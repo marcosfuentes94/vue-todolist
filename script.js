8abbad8cd620e96c335b215ac62dfe57aeb62a52
marcosfuentes94 <marcoslionel@hotmail.it>
2023-07-04 15:24:05 +0200

    milestone 3 completato

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const app = Vue.createApp({
     data() {
-      return {
-        todos: [
-          { id: 1, text: 'Fare la spesa', done: false },
-          { id: 2, text: 'Pulire la casa', done: true },
-          { id: 4, text: 'Studiare Vue.js', done: false },
-          { id: 5, text: 'Lavare la macchina.js', done: false },
-          { id: 6, text: 'Preparare meme per Alessio', done: false },
-        ]
-      }
+        return {
+            todos: [
+                { id: 1, text: 'Fare la spesa', done: false },
+                { id: 2, text: 'Pulire la casa', done: true },
+                { id: 4, text: 'Studiare Vue.js', done: false },
+                { id: 5, text: 'Lavare la macchina', done: false },
+                { id: 6, text: 'Preparare meme per Alessio', done: false },
+            ],
+            newTodoText: ''
+        };
     },
-  methods: {
-    removeTodo(todoId) {
-      this.todos = this.todos.filter(todo => todo.id !== todoId);
+    methods: {
+        addTodo() {
+            if (this.newTodoText.trim() !== '') {
+                const newTodo = {
+                    id: Date.now(),
+                    text: this.newTodoText,
+                    done: false
+                };
+                this.todos.push(newTodo);
+                this.newTodoText = '';
+            }
+        },
+        toggleTodoDone(todo) {
+            todo.done = !todo.done;
+        }
     }
-  }
-  });
+});
 
-  app.mount('#app');
+app.mount('#app');

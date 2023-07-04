fbb59cf9c3fb71bd8ad9e7ef490212a0af428cf0
marcosfuentes94 <marcoslionel@hotmail.it>
2023-07-04 14:47:58 +0200

    milestone 1 completato

---
+++ script.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        todos: [
+          { id: 1, text: 'Fare la spesa', done: false },
+          { id: 2, text: 'Pulire la casa', done: true },
+          { id: 4, text: 'Studiare Vue.js', done: false },
+          { id: 5, text: 'Lavare la macchina.js', done: false },
+          { id: 6, text: 'Preparare meme per Alessio', done: false },
+        ]
+      }
+    }
+  });
+
+  app.mount('#app');
